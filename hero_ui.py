b44c6eb74cef8483443f2fbadf1ef15cf5a9a00d
DavidBartTobi <82588072+DavidBartTobi@users.noreply.github.com>
2022-03-20 23:42:05 +0200

    Update hero_ui.py

--- hero_ui.py
+++ hero_ui.py
@@ -4,7 +4,7 @@ from PIL import ImageTk,Image
 import modifications
 
 
-class hero_ui:
+class Hero_ui:
 
     def __init__(self, root, h1, h2, h3, h4, h5, h6, h7 ,h8, column, hero_id):
 
@@ -79,75 +79,75 @@ class hero_ui:
 
         self.h1_entry = Entry(self.root, width=13, bg=entry_bg, fg=fg, font=font, justify=CENTER)
         self.h1_entry.grid(row=2, column=column+1, padx=30, pady=(10, 0))
-        self.h1_entry.insert(0, modifications.Get_Sensitivity(self.hero_id))
+        self.h1_entry.insert(0, modifications.get_sensitivity(self.hero_id))
         self.hero_id += 1
 
         self.h2_entry = Entry(self.root, width=13, bg=entry_bg, fg=fg, font=font, justify=CENTER)
         self.h2_entry.grid(row=4, column=column+1, padx=30, pady=(10, 0))
-        self.h2_entry.insert(0, modifications.Get_Sensitivity(self.hero_id))
+        self.h2_entry.insert(0, modifications.get_sensitivity(self.hero_id))
         self.hero_id += 1
 
         self.h3_entry = Entry(self.root, width=13, bg=entry_bg, fg=fg, font=font, justify=CENTER)
         self.h3_entry.grid(row=6, column=column+1, padx=30, pady=(10, 0))
-        self.h3_entry.insert(0, modifications.Get_Sensitivity(self.hero_id))
+        self.h3_entry.insert(0, modifications.get_sensitivity(self.hero_id))
         self.hero_id += 1
 
         self.h4_entry = Entry(self.root, width=13, bg=entry_bg, fg=fg, font=font, justify=CENTER)
         self.h4_entry.grid(row=8, column=column+1, padx=30, pady=(10, 0))
-        self.h4_entry.insert(0, modifications.Get_Sensitivity(self.hero_id))
+        self.h4_entry.insert(0, modifications.get_sensitivity(self.hero_id))
         self.hero_id += 1
 
         self.h5_entry = Entry(self.root, width=13, bg=entry_bg, fg=fg, font=font, justify=CENTER)
         self.h5_entry.grid(row=10, column=column+1, padx=30, pady=(10, 0))
-        self.h5_entry.insert(0, modifications.Get_Sensitivity(self.hero_id))
+        self.h5_entry.insert(0, modifications.get_sensitivity(self.hero_id))
         self.hero_id += 1
 
         self.h6_entry = Entry(self.root, width=13, bg=entry_bg, fg=fg, font=font, justify=CENTER)
         self.h6_entry.grid(row=12, column=column+1, padx=30, pady=(10, 0))
-        self.h6_entry.insert(0, modifications.Get_Sensitivity(self.hero_id))
+        self.h6_entry.insert(0, modifications.get_sensitivity(self.hero_id))
         self.hero_id += 1
 
         self.h7_entry = Entry(self.root, width=13, bg=entry_bg, fg=fg, font=font, justify=CENTER)
         self.h7_entry.grid(row=14, column=column+1, padx=30, pady=(10, 0))
-        self.h7_entry.insert(0, modifications.Get_Sensitivity(self.hero_id))
+        self.h7_entry.insert(0, modifications.get_sensitivity(self.hero_id))
         self.hero_id += 1
 
         self.h8_entry = Entry(self.root, width=13, bg=entry_bg, fg=fg, font=font, justify=CENTER)
         self.h8_entry.grid(row=16, column=column+1, padx=30, pady=(10, 0))
-        self.h8_entry.insert(0, modifications.Get_Sensitivity(self.hero_id))
+        self.h8_entry.insert(0, modifications.get_sensitivity(self.hero_id))
 
         
         button_bg = '#F9B551'
         
-        h1_button = Button(self.root, text='Modify', command=lambda: modifications.Hero_Modify(self.h1_entry.get(), self.hero_id-7),
+        h1_button = Button(self.root, text='Modify', command=lambda: modifications.hero_modify(self.h1_entry.get(), self.hero_id-7),
                            bg=button_bg)
         h1_button.grid(row=3, column=column+1, pady=(0, 10))
 
-        h2_button = Button(self.root, text='Modify', command=lambda: modifications.Hero_Modify(self.h2_entry.get(), self.hero_id-6),
+        h2_button = Button(self.root, text='Modify', command=lambda: modifications.hero_modify(self.h2_entry.get(), self.hero_id-6),
                            bg=button_bg)
         h2_button.grid(row=5, column=column+1, pady=(0, 10))
 
-        h3_button = Button(self.root, text='Modify', command=lambda: modifications.Hero_Modify(self.h3_entry.get(), self.hero_id-5),
+        h3_button = Button(self.root, text='Modify', command=lambda: modifications.hero_modify(self.h3_entry.get(), self.hero_id-5),
                                  bg=button_bg)
         h3_button.grid(row=7, column=column+1, pady=(0, 10))
 
-        h4_button = Button(self.root, text='Modify', command=lambda: modifications.Hero_Modify(self.h4_entry.get(), self.hero_id-4),
+        h4_button = Button(self.root, text='Modify', command=lambda: modifications.hero_modify(self.h4_entry.get(), self.hero_id-4),
                                 bg=button_bg)
         h4_button.grid(row=9, column=column+1, pady=(0, 10))
 
-        h5_button = Button(self.root, text='Modify', command=lambda: modifications.Hero_Modify(self.h5_entry.get(), self.hero_id-3),
+        h5_button = Button(self.root, text='Modify', command=lambda: modifications.hero_modify(self.h5_entry.get(), self.hero_id-3),
                                  bg=button_bg)
         h5_button.grid(row=11, column=column+1, pady=(0, 10))
 
-        h6_button = Button(self.root, text='Modify', command=lambda: modifications.Hero_Modify(self.h6_entry.get(), self.hero_id-2),
+        h6_button = Button(self.root, text='Modify', command=lambda: modifications.hero_modify(self.h6_entry.get(), self.hero_id-2),
                            bg=button_bg)
         h6_button.grid(row=13, column=column+1, pady=(0, 10))
 
-        h7_button = Button(self.root, text='Modify', command=lambda: modifications.Hero_Modify(self.h7_entry.get(), self.hero_id-1),
+        h7_button = Button(self.root, text='Modify', command=lambda: modifications.hero_modify(self.h7_entry.get(), self.hero_id-1),
                                  bg=button_bg)
         h7_button.grid(row=15, column=column+1, pady=(0, 10))
 
-        h8_button = Button(self.root, text='Modify', command=lambda: modifications.Hero_Modify(self.h8_entry.get(), self.hero_id),
+        h8_button = Button(self.root, text='Modify', command=lambda: modifications.hero_modify(self.h8_entry.get(), self.hero_id),
                            bg=button_bg)
         h8_button.grid(row=17, column=column+1, pady=(0, 25))
 
@@ -172,11 +172,11 @@ class hero_ui:
         self.h7_entry.insert(0, 0)
         self.h8_entry.insert(0, 0)
 
-        modifications.Hero_Modify(self.h1_entry.get(), self.hero_id - 7)
-        modifications.Hero_Modify(self.h2_entry.get(), self.hero_id - 6)
-        modifications.Hero_Modify(self.h3_entry.get(), self.hero_id - 5)
-        modifications.Hero_Modify(self.h4_entry.get(), self.hero_id - 4)
-        modifications.Hero_Modify(self.h5_entry.get(), self.hero_id - 3)
-        modifications.Hero_Modify(self.h6_entry.get(), self.hero_id - 2)
-        modifications.Hero_Modify(self.h7_entry.get(), self.hero_id - 1)
-        modifications.Hero_Modify(self.h8_entry.get(), self.hero_id)
+        modifications.hero_modify(self.h1_entry.get(), self.hero_id - 7)
+        modifications.hero_modify(self.h2_entry.get(), self.hero_id - 6)
+        modifications.hero_modify(self.h3_entry.get(), self.hero_id - 5)
+        modifications.hero_modify(self.h4_entry.get(), self.hero_id - 4)
+        modifications.hero_modify(self.h5_entry.get(), self.hero_id - 3)
+        modifications.hero_modify(self.h6_entry.get(), self.hero_id - 2)
+        modifications.hero_modify(self.h7_entry.get(), self.hero_id - 1)
+        modifications.hero_modify(self.h8_entry.get(), self.hero_id)
